166fade0f734e0637093a74521e5a1d230cb379d
Yan Pablo Alves <yanpabloalves@gmail.com>
2021-11-03 10:57:07 -0300

    create blocs, models and request the api to get pokemons

--- lib/src/blocs/GetPokemons/GetPokemonsBloc.dart
+++ lib/src/blocs/GetPokemons/GetPokemonsBloc.dart
@@ -18,21 +18,7 @@ class GetPokemonsBloc extends Bloc<GetPokemonsEvent, GetPokemonsState> {
     try {
       if (event is GetPokemonsEvent) {
         yield LoadingGetPokemonsState();
-
-        // var response = await ApiPokemons.pokemonList();
-
-        // if (response != null) {
-        //   for (var p in response) {
-        //     Poke pokemon = Poke.fromJson(p);
-        //     pokemons.add(pokemon);
-        //   }
-
-        //   yield SuccessGetPokemonsState(pokemons);
-        //   pokemons = [];
-        // } else {
-        //   yield ErrorGetPokemonsState(
-        //       "Não foi possível listar os pokémons, tente novamente mais tarde!");
-        // }
+        
         for (int i = 1; i < 100; i++) {
           var response = await ApiPokemons.getPokemon(i);
           Poke pokemon = Poke.fromJson(response);
--- lib/src/providers/Api+Pokemons.dart
+++ lib/src/providers/Api+Pokemons.dart
@@ -2,20 +2,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiPokemons {
-  // static Future<dynamic> pokemonList() async {
-  //   Uri url = Uri.https("pokeapi.co", "/api/v2/pokemon/",  {"limit":"800"});
-
-  //   print(url);
-
-  //   var response = await http.get(url);
-  //   if (response.statusCode == 200) {
-  //     var jsonResponse = json.decode(response.body) as Map<String, dynamic>;
-  //     return jsonResponse["results"];
-  //   } else {
-  //     return null;
-  //   }
-  // }
-
   static Future<dynamic> getPokemon(int id) async {
     Uri url = Uri.https(
       "pokeapi.co",
